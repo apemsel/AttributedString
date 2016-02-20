013eb9e767b9e4955233c2a224057d3d9a8c9842
Adrian Pemsel <ap@jusmeum.de>
2016-02-20 14:15:22 +0100

    test basic attribute methods

--- test/unit/AttributedStringTest.php
+++ test/unit/AttributedStringTest.php
@@ -6,5 +6,19 @@ class AttributedStringTest extends PHPUnit_Framework_TestCase
   public function testConstructAndToString() {
     $as = new AttributedString("foo");
     $this->assertEquals("foo", $as);
+    
+    $this->setExpectedException('InvalidArgumentException');
+    $as = new AttributedString(1);
+  }
+  
+  public function testBasicAttributes() {
+    $as = new AttributedString("foo");
+    $as->createAttribute("attribute");
+    
+    $this->assertEquals(true, $as->hasAttribute("attribute"));
+    $this->assertEquals(false, $as->hasAttribute("non-existing"));
+    
+    $as->deleteAttribute("attribute");
+    $this->assertEquals(false, $as->hasAttribute("attribute"));
   }
 }
