f3ff26d9e624acc56620cb7ddb798007c59bc0d7
Adrian Pemsel <ap@jusmeum.de>
2016-02-26 11:27:57 +0100

    more tests for TokenizedAttributedString->getTokens() and ->getTokenOffsets()

--- test/unit/TokenizedAttributedStringTest.php
+++ test/unit/TokenizedAttributedStringTest.php
@@ -9,7 +9,13 @@ class TokenizedAttributedStringTest extends PHPUnit_Framework_TestCase
   }
   
   public function testGetTokens() {
-    $as = new TokenizedAttributedString("foo bar baz");
-    $this->assertEquals(["foo", "bar", "baz"], $as->getTokens());    
+    $as = new TokenizedAttributedString(" one two\nthree\rfour\n\r five  ");
+    $this->assertEquals(["one", "two", "three", "four", "five"], $as->getTokens());
   }
+  
+  public function testGetTokenOffsets() {
+    $as = new TokenizedAttributedString(" one two\nthree\rfour\n\r five  ");
+    $this->assertEquals([1, 5, 9, 15, 22], $as->getTokenOffsets());
+  }
+
 }
