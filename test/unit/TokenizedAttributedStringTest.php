9ba7efaeb5241279fba3880c142db2bd3bbf86c3
Adrian Pemsel <ap@jusmeum.de>
2016-02-29 09:52:57 +0100

    tests for TokenizedAttributedString custom tokenizer

--- test/unit/TokenizedAttributedStringTest.php
+++ test/unit/TokenizedAttributedStringTest.php
@@ -7,11 +7,17 @@ class TokenizedAttributedStringTest extends PHPUnit_Framework_TestCase
     $as = new TokenizedAttributedString("foo bar baz");
     $this->assertEquals("foo bar baz", $as);
   }
-  
+
   public function testGetTokens() {
     $as = new TokenizedAttributedString(" one two\nthree\rfour\n\r five  ");
     $this->assertEquals(["one", "two", "three", "four", "five"], $as->getTokens());
   }
+  
+  public function testPatternTokenizer() {
+    $as = new TokenizedAttributedString("   ?foo, !bar. \n baz", "/([\w]+)/u");
+    $this->assertEquals(["foo", "bar", "baz"], $as->getTokens());
+  }
+  
   
   public function testGetTokenOffsets() {
     $as = new TokenizedAttributedString(" one two\nthree\rfour\n\r five  ");
