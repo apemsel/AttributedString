fbdee1fb5042d4211db447d0c4597d67dac801a5
Adrian Pemsel <apemsel@gmail.com>
2016-03-12 18:57:04 +0100

    add tests for Bitmap class

---
+++ test/unit/BitmapTest.php
@@ -0,0 +1,21 @@
+<?php
+use apemsel\AttributedString\Bitmap;
+
+class BitmapTest extends PHPUnit_Framework_TestCase
+{
+  public function testArrayAccess() {
+    $b = new Bitmap(10);
+    $b[0] = true;
+    $b[9] = true;
+    $this->assertEquals(true, $b[0]);
+    $this->assertEquals(true, $b[9]);
+    
+    $b[9] = false;
+    $this->assertEquals(false, $b[9]);
+    
+    $this->setExpectedException('RuntimeException');
+    unset($b[0]);
+  }
+  
+  
+}
