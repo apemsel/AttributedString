b5445c37a935593db7eb8d8929cea753bae9306f
Adrian Pemsel <apemsel@gmail.com>
2016-03-12 18:57:22 +0100

    begin implementing Bitmap class

---
+++ src/Bitmap.php
@@ -0,0 +1,61 @@
+<?php
+namespace apemsel\AttributedString;
+
+class Bitmap implements \ArrayAccess
+{
+  protected $bitmap;
+  protected $length;
+  
+  public function __construct($length) {
+    $this->length = $length;
+    $this->bitmap = str_repeat("\0", ceil($this->length / 8));
+  }
+  
+  public function __toString() {
+    return $this->toString();
+  }
+  
+  public function toString($true = "1", $false = "0") {
+    $string = str_repeat($false, $this->length);
+    for ($offset = 0; $offset < $this->length; $offset++) {
+      if (ord($this->bitmap[(int) ($offset / 8)]) & (1 << $offset % 8)) {
+        $string[$offset] = $true;
+      }
+    }
+    
+    return $string;
+  }
+  
+  // ArrayAccess interface
+  
+  public function offsetExists($offset) {
+    return is_int($offset) && $offset >= 0 && $offset < $this->length;
+  }
+  
+  public function offsetGet($offset)
+	{
+		if ($this->offsetExists($offset)) {
+			return (bool) (ord($this->bitmap[(int) ($offset / 8)]) & (1 << $offset % 8));
+		} else {
+			throw new \OutOfRangeException();
+		}
+	}
+  
+  public function offsetSet($offset, $value)
+	{
+		if ($this->offsetExists($offset)) {
+			$index = (int) ($offset / 8);
+			if ($value) {
+				$this->bitmap[$index] = chr(ord($this->bitmap[$index]) | (1 << $offset % 8));
+			} else {
+				$this->bitmap[$index] = chr(ord($this->bitmap[$index]) & ~(1 << $offset % 8));
+			}
+		} else {
+			throw new \OutOfRangeException();
+		}
+	}
+  
+  public function offsetUnset($offset) {
+    throw new \RuntimeException("Bitmap does not support offsetUnset");
+  }
+}
