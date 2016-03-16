012fb75b2f3fd35e04e683c4e89b48320ed8810d
Adrian Pemsel <apemsel@gmail.com>
2016-03-16 10:34:54 +0100

    update documentation

--- doc/elementlist.js
+++ doc/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","apemsel\\AttributedString\\AttributedString"],["c","apemsel\\AttributedString\\MutableAttributedString"],["c","apemsel\\AttributedString\\TokenizedAttributedString"]];
+ApiGen.elements = [["c","apemsel\\AttributedString\\Attribute"],["c","apemsel\\AttributedString\\AttributedString"],["c","apemsel\\AttributedString\\Bitmap"],["c","apemsel\\AttributedString\\BooleanArray"],["c","apemsel\\AttributedString\\MutableAttribute"],["c","apemsel\\AttributedString\\MutableAttributedString"],["c","apemsel\\AttributedString\\TokenizedAttributedString"]];
