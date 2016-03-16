a17108252bdc66c3fc480db48854c24d7044419a
Adrian Pemsel <apemsel@gmail.com>
2016-03-16 10:16:22 +0100

    Add MutableAttribute interface

---
+++ src/MutableAttribute.php
@@ -0,0 +1,8 @@
+<?php
+namespace apemsel\AttributedString;
+
+interface MutableAttribute extends Attribute
+{
+  public function insert($offset, $length, $state);
+  public function delete($offset, $length);
+}
